21e66d03209f9d85e629d2d63018ec633c99e2f9
farrelfaiq_ <farrelruliff165@gmail.com>
2025-06-18 18:14:39 +0700

    update 2

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./public/**/*.html",
+            "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: ['"Share Tech Mono"', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+}
